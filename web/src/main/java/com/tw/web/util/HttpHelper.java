06499685607f2ba3c7012f9824fef4a236705e91
blossom <408267415@qq.com>
2015-07-16 23:16:35 +0800

    TML-2015-0716  add log4j to project

--- web/src/main/java/com/tw/web/util/HttpHelper.java
+++ web/src/main/java/com/tw/web/util/HttpHelper.java
@@ -1,8 +1,5 @@
 package com.tw.web.util;
 
-
-import com.tw.core.service.userService.UserService;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class HttpHelper extends HttpServlet {
-    UserService personService = new UserService();
+    //UserService personService = new UserService();
     public void doGetHelper (HttpServletRequest req, HttpServletResponse res, String jsp) throws ServletException, IOException {
 //        res.setContentType("text/html;charset=UTF-8");
 //        res.setCharacterEncoding("utf-8");
